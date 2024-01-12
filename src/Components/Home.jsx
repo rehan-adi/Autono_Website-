4c22cb4a09fdefb3af9f4c716d77774c2a0323f5
Rehan <rehanalire52@gmail.com>
2024-01-12 19:53:45 +0530

    update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
                 
              </div>
              <div>
-                 <h1 className='uppercase mb-10 text-base'>Vision</h1>
+                 <h1 className='mb-10 text-base tracking-widest'>VISION</h1>
                  <h2 className='home-line1 mb-10 text-2xl tracking-widest'>We’re Changing the <br /> Way the World Thinks <br /> About Cars</h2>
                  <p className='home-line2 tracking-widest'>I'm a paragraph. Click here to add your <br /> own text and edit me. It’s easy. Just <br /> click “Edit Text” or double click me to <br /> add your own content and make <br /> changes to the font. I’m a great place <br /> for you to tell a story and let your users <br /> know a little more about you.</p>
              </div>
@@ -30,7 +30,7 @@ function Home() {
                 
              </div>
              <div>
-                 <h1 className='uppercase mb-10 text-base'>SERVICES</h1>
+                 <h1 className='uppercase mb-10 tracking-widest text-base'>SERVICES</h1>
                  <h2 className='home-line1 mb-10 text-2xl tracking-widest'>We Deliver Exceptional <br /> Products and Services <br /> Around the World</h2>
                  <h1 className='text-4xl home-line2'>AUTONOMOUS <br />DRIVING</h1>
                  <p className='home-line2 mt-6 tracking-widest'>I'm a paragraph. Click here to add your <br /> own text and edit me. It’s easy. Just <br /> click “Edit Text” or double click me to <br /> add your own content and make <br /> changes to the font. I’m a great place <br /> for you to tell a story and let your users <br /> know a little more about you.</p>
@@ -45,12 +45,114 @@ function Home() {
           <div className="max-w-full">
           <img className="max-w-full h-auto" src="https://static.wixstatic.com/media/84770f_abca8ce0bac24971a57d1f9d40b8238b~mv2.jpg/v1/fill/w_950,h_850,al_r,q_85,usm_0.66_1.00_0.01,enc_auto/84770f_abca8ce0bac24971a57d1f9d40b8238b~mv2.jpg" alt="img" width={700} />
         </div>
-             <div className='pt-32 ml-36'>
+             <div className='pt-32 ml-44'>
                  <h1 className='text-4xl home-line2'>REAL-TIME <br /> INFORMATION</h1>
                  <p className='home-line2 mt-6 tracking-widest'>I'm a paragraph. Click here to add your <br /> own text and edit me. It’s easy. Just <br /> click “Edit Text” or double click me to <br /> add your own content and make <br /> changes to the font. I’m a great place <br /> for you to tell a story and let your users <br /> know a little more about you.</p>
                  <button className='bg-transparent border hover:bg-black hover:text-white duration-500  border-gray-600 px-6 py-1 mt-7 text-black rounded-lg'>Read More</button>
              </div>
              </div>
+        </div>
+        <div className='flex items-center h-screen justify-center bg-white text-black'>
+          <div className='flex'>
+          <div className='pt-32 mr-44'>
+                 <h1 className='text-4xl home-line2'>PERCEPTION <br /> ENABLED</h1>
+                 <p className='home-line2 mt-6 tracking-widest'>I'm a paragraph. Click here to add your <br /> own text and edit me. It’s easy. Just <br /> click “Edit Text” or double click me to <br /> add your own content and make <br /> changes to the font. I’m a great place <br /> for you to tell a story and let your users <br /> know a little more about you.</p>
+                 <button className='bg-transparent border hover:bg-black hover:text-white duration-500  border-gray-600 px-6 py-1 mt-7 text-black rounded-lg'>Read More</button>
+             </div>
+          <div className="max-w-full">
+          <img className="max-w-full h-auto" src="https://static.wixstatic.com/media/c837a6_305ff8a7722a479ab58f1d4f2729586e~mv2.jpg/v1/fill/w_950,h_850,al_l,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_305ff8a7722a479ab58f1d4f2729586e~mv2.jpg" alt="img" width={700} />
+        </div>
+             </div>
+        </div>
+        <div className='lawra-img w-auto flex justify-start  items-center h-[110vh]'>
+              <div className='bg-black w-[30vw] text-white flex justify-center items-center rounded-xl ml-20 h-[80vh]'>
+              <div className='flex '>
+             <div className='border-l border-white opacity-60 pr-10'>
+                
+             </div>
+             <div>
+                 <h1 className='mb-10 text-base tracking-widest'>WHY AUTONO</h1>
+                 <h2 className='home-line1 mb-10 text-2xl tracking-widest'>A different approach, <br /> using a new method of <br /> manufacturing.</h2>
+                 <p className='home-line2 tracking-widest'>I'm a paragraph. Click here to add your <br /> own text and edit me. It’s easy. Just <br /> click “Edit Text” or double click me to <br /> add your own content and make <br /> changes to the font. I’m a great place <br /> for you to tell a story and let your users <br /> know a little more about you.</p>
+             </div>
+             </div>
+              </div>
+        </div>
+        <div className='bg-white flex justify-start items-center w-auto h-[100vh]'>
+                <div>
+                    <img src="https://static.wixstatic.com/media/c837a6_3a4f054db4424af1b034f96ddd7b691a~mv2.jpg/v1/fill/w_950,h_950,al_r,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_3a4f054db4424af1b034f96ddd7b691a~mv2.jpg" width={730} alt="img" />
+                </div>
+                <div className='flex items-center justify-center'>
+                <div className='ml-40'>
+                   <h1 className='text-3xl tracking-wider home-line2'>Autono In Numbers</h1>
+                   <div className='mt-16'>
+                      <h1 className='text-4xl font-normal'>200</h1>
+                      <div className='border-t mt-5 w-44 border-black opacity-70'>
+
+                      </div>
+                      <h4 className='mt-4 tracking-widest'>EMPLOYEES</h4>
+                   </div>
+                   <div className='mt-20'>
+                      <h1 className='text-4xl font-normal'>200M$</h1>
+                      <div className='border-t mt-5 w-44 border-black opacity-70'>
+
+                      </div>
+                      <h4 className='mt-4 tracking-widest'>CAPITAL</h4>
+                   </div>
+                </div>
+                </div>
+                <div className='flex'>
+                <div className='mt-10'>
+                   <div className='mt-16'>
+                      <h1 className='text-4xl font-normal'>5</h1>
+                      <div className='border-t mt-5 w-44 border-black opacity-70'>
+
+                      </div>
+                      <h4 className='mt-4 tracking-widest'>CORE TEAMS</h4>
+                   </div>
+                   <div className='mt-20'>
+                      <h1 className='text-4xl font-normal'>326</h1>
+                      <div className='border-t mt-5 w-44 border-black opacity-70'>
+
+                      </div>
+                      <h4 className='mt-4 tracking-widest'>PARTNERS</h4>
+                   </div>
+                </div>
+                </div>
+        </div>
+        <div className='flex items-center justify-between h-[90vh] w-auto px-36 bg-black text-white'>
+          <div className='flex '>
+             <div className='border-l border-white opacity-60 pr-10'>
+                
+             </div>
+             <div>
+                 <h1 className='mb-10 text-base tracking-widest'>INDUSTRY</h1>
+                 <h2 className='home-line1 mb-10 text-2xl tracking-widest'>Our <br />Partners</h2>
+                 <p className='home-line2 tracking-widest'>Click here to add your own content <br /> and customize the text. This is a great <br /> place to tell a story about your <br /> company and let your users know a <br /> little more about the company's <br /> history, the team's background, or any <br /> other information you'd like to share. <br /> Just click "Edit Text" to get started.</p>
+             </div>
+             </div>
+             <div className='flex  flex-col gap-20 justify-center items-center'>
+             <div className='flex gap-20 justify-center items-center'>
+             <div className="max-w-full flex items-center justify-center flex-col text-center">
+          <img className="max-w-full h-auto" src="https://static.wixstatic.com/media/c837a6_a8508672733a4bb7ba100662952d4f6f~mv2.png/v1/fill/w_175,h_175,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/General_Transport.png" alt="" width={140} />
+          <h2 className='tracking-widest text-center text-base'>GENERAL TRANSPORT</h2>
+        </div>
+             <div className="max-w-full flex items-center justify-center flex-col text-center">
+          <img className="max-w-full mt-7 h-auto" src="https://static.wixstatic.com/media/c837a6_24669e1d20cd4f079fd2ea5e253c99b8~mv2.png/v1/fill/w_110,h_80,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/IDI_Software.png" alt="" width={100} />
+          <h2 className='tracking-widest mt-10 text-center text-base'>IDI SOFTWARE</h2>
+        </div>
+        </div>
+             <div className='flex gap-20 justify-center items-center'>
+             <div className="max-w-full flex items-center justify-center flex-col text-center">
+          <img className="max-w-full h-auto" src="https://static.wixstatic.com/media/c837a6_db1c6e9c1b95497f81465ccd218fa048~mv2.png/v1/fill/w_110,h_100,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Imogen_Cars.png" alt="" width={90} />
+          <h2 className='tracking-widest text-center mt-8 text-base'>IMOGEN CARS</h2>
+        </div>
+             <div className="max-w-full flex items-center justify-center flex-col text-center">
+          <img className="max-w-full h-auto" src="https://static.wixstatic.com/media/c837a6_81bc98e386764170adbb3e97cc7e1655~mv2.png/v1/fill/w_160,h_80,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Tri-Nex%20.png" alt="" width={140} />
+          <h2 className='tracking-widest mt-26 mt-10 text-center text-base'>TRI-NEX</h2>
+        </div>
+        </div>
+        </div>
         </div>
     </main>
   )
