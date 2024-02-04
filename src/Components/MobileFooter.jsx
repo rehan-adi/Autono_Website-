7276869eca90c1226c7c07a937286fe05c7071b0
Rehan <rehanalire52@gmail.com>
2024-02-04 22:43:40 +0530

    Update on MobileFotter

--- src/Components/MobileFooter.jsx
+++ src/Components/MobileFooter.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { FaLinkedinIn } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
 
 function MobileFooter() {
   return (
-    <div className='h-screen lg:hidden flex justify-center items-center w-auto bg-white text-black'>
+    <div className='h-[90vh] lg:hidden flex justify-center items-center w-auto bg-white text-black'>
           <div className='text-center'>
           <h1 className='font-bold tracking-wide text-xl'><span className='mr-2'>A</span><span className='mr-2'>U</span><span className='mr-2'>T</span><span className='mr-2'>O</span><span className='mr-2'>N</span>O</h1>
           <div>
@@ -22,6 +26,15 @@ function MobileFooter() {
                     </li>
                  </ul>
           </div>
+          <div className='flex justify-center gap-8 mt-10 items-center'>
+                 <h1 className='text-sm'>© 2035 by Autono.<span className='underline'>Wix</span></h1>
+                  <div className='flex items-center gap-5'>
+                     <span><FaLinkedinIn  className='text-base text-black'/></span>
+                     <span><FiInstagram  className='text-base text-black'/></span>
+                     <span><FaFacebookF className='text-base text-black' /></span>
+                     <span><FaTwitter  className='text-base text-black'/></span>
+                  </div>
+              </div>
           </div>
     </div>
   )
