9154fb5b259c811a92f7c6ff210dd9a8859c4811
Rehan <rehanalire52@gmail.com>
2024-02-05 11:52:58 +0530

    Update on About page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -59,16 +59,18 @@ function About() {
 
 
   return (
-    <div className='h-[370vh] w-auto'>
-         <div className='h-[90vh] flex-col flex border-b border-black justify-center items-center w-auto bg-white'>
-                <h1 className='text-7xl pt-20 tracking-widest  text-center home-line font-normal'>OUR JOURNEY</h1>
-                <p className='text-2xl text-center home-line1 tracking-wider mt-14'>I'm a paragraph. Click here to add your own text and <br /> edit me. It’s easy. Just click “Edit Text” or double click <br /> me to add your own content and make changes to the <br /> font. I’m a great place for you to tell a story and let <br /> your users know a little more about you.</p>
+    <div className=' h-[420vh] lg:h-[370vh] w-auto'>
+         <div className=' lg:h-[90vh] h-[80vh] flex-col flex border-b border-black justify-center items-center w-auto bg-white'>
+                <h1 className=' text-5xl lg:text-7xl pt-20 tracking-widest text-center home-line font-normal'>OUR JOURNEY</h1>
+               <div className='w-[85vw] lg:w-[60vw] px-4'>
+               <p className=' text-sm  lg:text-2xl text-center home-line1 tracking-wider mt-14'>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+               </div>
          </div>
            
 
-         <div className='flex h-[90vh] border-b border-black w-auto px-36 items-center justify-between'>
+         <divvv className='flex h-[170vh] pb-28 lg:pb-0 pt-20 lg:pt-0 lg:h-[90vh] border-b border-black w-auto lg:px-36 flex-col lg:flex-row gap-16 lg:gap-0 items-center justify-between'>
           <div className='flex '>
-             <div className='border-l border-black opacity-60 pr-10'>
+             <div className='border-l border-black opacity-60 pr-6 lg:pr-10'>
                 
              </div>
              <div className=''>
@@ -76,28 +78,28 @@ function About() {
                  initial={{opacity: 0, x : -35}}
                  animate={controls1}
                  ref={ref}   
-                className='mb-8 text-base tracking-widest'>OUR MISSION</motion.h3>
+                className='mb-8 text-sm lg:text-base tracking-widest'>OUR MISSION</motion.h3>
                  <motion.h1
                   initial={{opacity: 0, x : -35}}
                   animate={controls2}
                   ref={ref}  
-                 className='mb-14 text-2xl home-line1  tracking-widest'>Redefining the Way <br /> We Move</motion.h1>
+                 className='mb-14 text-base lg:text-2xl home-line1 font-semibold  tracking-widest'>Redefining the Way <br /> We Move</motion.h1>
                 <motion.p
                  initial={{opacity: 0, x : -35}}
                  animate={controls3}
                  ref={ref}   
-                className='home-line2 tracking-widest'>I'm a paragraph. Click here to add your <br /> own text and edit me. It’s easy. Just <br /> click “Edit Text” or double click me to <br /> add your own content and make <br /> changes to the font. I’m a great place <br /> for you to tell a story and let your users <br /> know a little more about you.</motion.p>
+                className='home-line2 lg:text-base text-xs tracking-widest'>I'm a paragraph. Click here to add your <br /> own text and edit me. It’s easy. Just <br /> click “Edit Text” or double click me to <br /> add your own content and make <br /> changes to the font. I’m a great place <br /> for you to tell a story and let your users <br /> know a little more about you.</motion.p>
                 <motion.p
                   initial={{opacity: 0, x : -35}}
                   animate={controls4}
                   ref={ref}    
-                className='home-line2 mt-6 tracking-widest'>This is a great space to write a long <br /> text about your  company and your <br /> services. You can use this space to go <br /> into a little more detail about your <br /> company. Talk about your team and <br /> what services you provide. </motion.p>
+                className='home-line2 mt-6 lg:text-base text-xs tracking-widest'>This is a great space to write a long <br /> text about your  company and your <br /> services. You can use this space to go <br /> into a little more detail about your <br /> company. Talk about your team and <br /> what services you provide. </motion.p>
              </div>
              </div>
              <div className="max-w-full ">
-          <img className="max-w-full rounded-lg  h-auto" src="https://static.wixstatic.com/media/c837a6_6805a01e59934a1584ecebb1a8ecd14c~mv2.jpg/v1/crop/x_3725,y_1163,w_2973,h_4576/fill/w_500,h_770,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/GettyImages-932691626.jpg" alt="" width={380} />
+          <img className="max-w-full lg:w-[380px] mr-4 lg:mr-0 rounded-lg  h-auto" src="https://static.wixstatic.com/media/c837a6_6805a01e59934a1584ecebb1a8ecd14c~mv2.jpg/v1/crop/x_3725,y_1163,w_2973,h_4576/fill/w_500,h_770,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/GettyImages-932691626.jpg" alt="" width={240} />
         </div>
-        </div>
+        </divvv>
         <div className='lawra1-img  h-[90vh] w-auto'>
 
         </div>
