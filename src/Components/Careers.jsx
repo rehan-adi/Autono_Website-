2899d87a19758fc8abbd8b8291a3c8c4b7d312d4
Rehan <rehanalire52@gmail.com>
2024-02-05 14:59:07 +0530

    Update Done

--- src/Components/Careers.jsx
+++ src/Components/Careers.jsx
@@ -117,13 +117,13 @@ function Careers() {
 
 
   return (
-    <div className='h-[430vh] w-auto'>
+    <div className='lg:h-[430vh] h-[560vh] w-auto'>
          <div className='h-[90vh] flex-col flex border-b border-black justify-center items-center w-auto bg-white'>
                 <h1 className='text-7xl pt-20 tracking-widest  text-center home-line font-normal'>WE MAKE <br /> BIG IDEAS HAPPEN</h1>
                 <p className='text-2xl tracking-widest mt-14'>Join Us for the Ride</p>
          </div>
          <div className='w-auto h-[100vh] lg:h-[90vh] bg-white'>
-         <divvv className='flex items-center flex-col pt-20 lg:flex-row justify-between lg:px-36 text-black'>
+         <div className='flex items-center flex-col pt-20 lg:flex-row justify-between lg:px-36 text-black'>
           <div className='flex '>
              <div className='border-l border-black opacity-60 pr-6 lg:pr-10'>
                 
@@ -149,9 +149,9 @@ function Careers() {
              <div className="max-w-full  text-center">
           <img className="max-w-full mr-12 lg:mr-0 lg:w-[650px] h-auto" src="https://static.wixstatic.com/media/84770f_b277d32c4552480eb4068ee023b6402d~mv2.jpg/v1/fill/w_950,h_950,al_l,q_85,usm_0.66_1.00_0.01,enc_auto/84770f_b277d32c4552480eb4068ee023b6402d~mv2.jpg" alt="" width={290} />
         </div>
-        </divvv>
+        </div>
          </div>
-         <div className='bg-black w-auto h-[250vh]'>
+         <div className='bg-black w-auto h-[400vh] lg:h-[250vh]'>
          <div className='flex items-center flex-col lg:flex-row justify-between lg:px-36 text-white'>
           <div className='flex pt-20 lg:pt-0 '>
              <div className='border-l border-white opacity-60 pr-6 lg:pr-10'>
@@ -179,96 +179,96 @@ function Careers() {
           <img className="max-w-full lg:w-[700px] h-auto" src="https://static.wixstatic.com/media/c837a6_dd610e0724af42ebbc47147f9fbbedf1~mv2.jpg/v1/fill/w_950,h_950,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_dd610e0724af42ebbc47147f9fbbedf1~mv2.jpg" alt="" width={310} />
         </div>
         </div>
-        <div className='flex items-center justify-around'>
-             <div className='bg-transparent flex pl-7 justify-start items-start flex-col text-white border h-[65vh] rounded-lg w-[27vw]'>
+        <div className='flex items-center flex-col pt-0 lg:pt-10 gap-12 lg:gap-0 lg:flex-row justify-around'>
+             <div className='bg-transparent flex pl-7 justify-start items-start flex-col text-white border lg:h-[65vh] h-[55vh] rounded-lg w-[80vw] lg:w-[27vw]'>
              <motion.h1
                initial={{opacity: 0, x : -35}}
                animate={Hero1}
                ref={ref2}   
-             className='mb-12 text-lg mt-16 tracking-widest'>DATA SCIENTIST</motion.h1>
+             className='mb-12 text-sm lg:text-lg mt-16 tracking-widest'>DATA SCIENTIST</motion.h1>
                      <motion.h4
                       initial={{opacity: 0, x : -35}}
                       animate={Hero2}
                       ref={ref2}  
-                     className='text-sm opacity-70 tracking-widest'>Silicon Valley</motion.h4>
+                     className='text-base lg:text-sm opacity-70 tracking-widest'>Silicon Valley</motion.h4>
                      <motion.p
                        initial={{opacity: 0, x : -35}}
                        animate={Hero3}
                        ref={ref2}   
-                     className='mt-5 home-line1'>I'm a paragraph. Click here to add your <br /> own text and edit me. It’s easy. Just <br /> click “Edit Text” or double click me to <br /> add your own content and make <br /> changes to the font. </motion.p>
+                     className='mt-5 text-xs lg:text-base home-line1'>I'm a paragraph. Click here to add your <br /> own text and edit me. It’s easy. Just <br /> click “Edit Text” or double click me to <br /> add your own content and make <br /> changes to the font. </motion.p>
                      <motion.button
                      initial={{opacity: 0,}}
                      animate={Hero7}
                      ref={ref2}    
-                     className='px-6 mt-10 py-2 hover:bg-white hover:text-black duration-500 border rounded-md'>Apply Now <span><MdArrowForward className='inline-block' /></span></motion.button>
+                     className='lg:px-6 px-4 mt-10 py-2 hover:bg-white text-sm lg:text-lg hover:text-black duration-500 border rounded-md'>Apply Now <span><MdArrowForward className='inline-block' /></span></motion.button>
              </div>
-             <div className='bg-transparent text-white pl-7  border h-[65vh] rounded-lg w-[27vw]'>
+             <div className='bg-transparent text-white pl-7  border h-[55vh] lg:h-[65vh] w-[80vw] rounded-lg lg:w-[27vw]'>
              <motion.h1
                 initial={{opacity: 0, x : 35}}
                 animate={Hero4}
                 ref={ref2} 
-             className='mb-12 text-lg mt-16 tracking-widest'>ELECTRICAL ENGINEER</motion.h1>
+             className='mb-12 text-sm lg:text-lg mt-16 tracking-widest'>ELECTRICAL ENGINEER</motion.h1>
                      <motion.h4
                       initial={{opacity: 0, x : 35}}
                       animate={Hero5}
                       ref={ref2}  
-                     className='text-sm opacity-70 tracking-widest'>San Francisco, CA</motion.h4>
+                     className='text-base lg:text-sm opacity-70 tracking-widest'>San Francisco, CA</motion.h4>
                      <motion.p
                         initial={{opacity: 0, x : 35}}
                         animate={Hero6}
                         ref={ref2} 
-                     className='mt-5 home-line1'>I'm a paragraph. Click here to add your <br /> own text and edit me. It’s easy. Just <br /> click “Edit Text” or double click me to <br /> add your own content and make <br /> changes to the font. </motion.p>
+                     className='mt-5 text-xs lg:text-base home-line1'>I'm a paragraph. Click here to add your <br /> own text and edit me. It’s easy. Just <br /> click “Edit Text” or double click me to <br /> add your own content and make <br /> changes to the font. </motion.p>
                      <motion.button
                       initial={{opacity: 0,}}
                       animate={Hero7}
                       ref={ref2}   
-                     className='px-6 mt-10 py-2 hover:bg-white hover:text-black duration-500 border rounded-md'>Apply Now <span><MdArrowForward className='inline-block' /></span></motion.button>
+                     className='lg:px-6 px-4 mt-10 py-2 text-sm lg:text-lg hover:bg-white hover:text-black duration-500 border rounded-md'>Apply Now <span><MdArrowForward className='inline-block' /></span></motion.button>
              </div>
         </div>
-        <div className='flex mt-24 items-center justify-around'>
-             <div className='bg-transparent text-white border pl-7 h-[65vh] rounded-lg w-[27vw]'>
+        <div className='flex mt-24 items-center flex-col lg:flex-row gap-12 lg:gap-0 justify-around'>
+             <div className='bg-transparent text-white border pl-7 h-[55vh] lg:h-[65vh] w-[80vw] rounded-lg lg:w-[27vw]'>
              <motion.h1
               initial={{opacity: 0, x : -35}}
               animate={A1}
               ref={ref3}  
-             className='mb-12 text-lg mt-16 tracking-widest'>ARTIFICIAL INTELLIGENCE <br /> RESEARCHER</motion.h1>
+             className='mb-12 text-sm lg:text-lg mt-16 tracking-widest'>ARTIFICIAL INTELLIGENCE <br /> RESEARCHER</motion.h1>
                      <motion.h4
                       initial={{opacity: 0, x : -35}}
                       animate={A2}
                       ref={ref3}   
-                     className='text-sm opacity-70 tracking-widest'>San Francisco, CA</motion.h4>
+                     className='text-base lg:text-sm opacity-70 tracking-widest'>San Francisco, CA</motion.h4>
                      <motion.p
                       initial={{opacity: 0, x : -35}}
                       animate={A3}
                       ref={ref3}   
-                     className='mt-5 home-line1'>I'm a paragraph. Click here to add your <br /> own text and edit me. It’s easy. Just <br /> click “Edit Text” or double click me to <br /> add your own content and make <br />  </motion.p>
+                     className='mt-5 text-xs lg:text-base home-line1'>I'm a paragraph. Click here to add your <br /> own text and edit me. It’s easy. Just <br /> click “Edit Text” or double click me to <br /> add your own content and make <br />  </motion.p>
                      <motion.button 
                       initial={{opacity: 0,}}
                       animate={A7}
                       ref={ref3}  
-                     className='px-6 mt-10 py-2 hover:bg-white hover:text-black duration-500 border rounded-md'>Apply Now <span><MdArrowForward className='inline-block' /></span></motion.button>
+                     className='lg:px-6 px-4 mt-10 py-2 text-sm lg:text-lg hover:bg-white hover:text-black duration-500 border rounded-md'>Apply Now <span><MdArrowForward className='inline-block' /></span></motion.button>
              </div>
-             <div className='bg-transparent text-white border pl-7 h-[65vh] rounded-lg w-[27vw]'>
+             <div className='bg-transparent text-white border pl-7 h-[55vh] w-[80vw] lg:h-[65vh] rounded-lg lg:w-[27vw]'>
              <motion.h1
                initial={{opacity: 0, x : 35}}
                animate={A4}
                ref={ref3}  
-             className='mb-12 text-lg mt-16 tracking-widest'>DEEP LEARNING ENGINEER</motion.h1>
+             className='mb-12 text-sm lg:text-lg mt-16 tracking-widest'>DEEP LEARNING ENGINEER</motion.h1>
                      <motion.h4
                       initial={{opacity: 0, x : 35}}
                       animate={A5}
                       ref={ref3}  
-                     className='text-sm opacity-70 tracking-widest'>San Francisco, CA</motion.h4>
+                     className='lg:text-sm text-base opacity-70 tracking-widest'>San Francisco, CA</motion.h4>
                      <motion.p
                       initial={{opacity: 0, x : 35}}
                       animate={A6}
                       ref={ref3}   
-                     className='mt-5 home-line1'>I'm a paragraph. Click here to add your <br /> own text and edit me. It’s easy. Just <br /> click “Edit Text” or double click me to <br /> add your own content and make <br /> changes to the font. </motion.p>
+                     className='mt-5 text-xs lg:text-base  home-line1'>I'm a paragraph. Click here to add your <br /> own text and edit me. It’s easy. Just <br /> click “Edit Text” or double click me to <br /> add your own content and make <br /> changes to the font. </motion.p>
                      <motion.button
                       initial={{opacity: 0,}}
                       animate={A7}
                       ref={ref3} 
-                     className='px-6 mt-10 py-2 hover:bg-white hover:text-black duration-500 border rounded-md'>Apply Now <span><MdArrowForward className='inline-block' /></span></motion.button>
+                     className='lg:px-6 px-4 mt-10 py-2 hover:bg-white text-sm lg:text-lg hover:text-black duration-500 border rounded-md'>Apply Now <span><MdArrowForward className='inline-block' /></span></motion.button>
              </div>
         </div>
          </div>
